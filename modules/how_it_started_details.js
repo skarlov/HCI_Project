b96c42c7e623d56191bc7bf837ff87e9b4c7c1bf
skarlov <56872071+skarlov@users.noreply.github.com>
2022-02-13 19:54:02 +0100

    Added text to how_it_started_details.js

--- modules/how_it_started_details.js
+++ modules/how_it_started_details.js
@@ -19,24 +19,15 @@ const HowItStartedDetails = () => {
                     </div>
                     <div className="mt-10 w-5/6 text-justify">
                         <p className="font-raleway leading-8">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing
-                            elit, sed do eiusmod tempor incididunt ut labore et
-                            dolore magna aliqua. Ut enim ad minim veniam, quis
-                            nostrud exercitation ullamco laboris nisi ut aliquip
-                            ex ea commodo consequat.
+                            I started Spooky Kitchen in 2011 with the intention of creating an online magazine featuring the web’s most innovative food bloggers, their recipes and photography. I wanted to take my years of creating magazines for others and make something of my own, to showcase the talents of so many food bloggers who with the simple click of a mouse inspire every level of home cook to create something wonderful in the kitchen.
+                            <br /><br />
+                            But then I discovered I couldn’t just feature food bloggers’ recipes like I did in my first post. I had to create my own recipes. I had to take my own photos. I had to become ONE OF THEM!
+                        </p>
+                        <p className="text-xl m-6 font-raleway font-bold text-center leading-8">
+                            And. So. I did.
                         </p>
                         <p className="font-raleway leading-8">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing
-                            elit, sed do eiusmod tempor incididunt ut labore et
-                            dolore magna aliqua. Ut enim ad minim veniam, quis
-                            nostrud exercitation ullamco laboris nisi ut aliquip
-                            ex ea commodo consequat.Ut enim ad minim veniam, quis
-                            nostrud exercitation ullamco laboris nisi ut aliquip
-                            ex ea commodo consequat.
-                        </p>
-                        <p className="text-xl m-6 font-raleway font-bold leading-8">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing
-                            elit.
+                            I worked at my magazine designing day job while blogging wee into the nights. Then in 2013, I quit corporate life to become a full time content creator of my own and I haven’t looked back. In case you’re wondering where the magazine went? FoodieCrush magazine has been on a hiatus while I dedicate more time to cultivating the community here. Because I love you guys!
                         </p>
                     </div>
 
@@ -51,25 +42,19 @@ const HowItStartedDetails = () => {
                         </div>
                         <div className="mt-10 w-5/6 text-justify">
                             <p className="font-raleway leading-8">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing
-                                elit, sed do eiusmod tempor incididunt ut labore et
-                                dolore magna aliqua. Ut enim ad minim veniam, quis
-                                nostrud exercitation ullamco laboris nisi ut aliquip
-                                ex ea commodo consequat.
+                                <b>HI, MY NAME IS LINDSAY!</b><br />
+                                And Spooky Kitchen is my little corner of the internet!
+                                <br /> <br />
+                                I'm the voice, author, and creator behind Spooky Kitchen. What started as a casual hobby for nights and weekends while I was working as a fourth grade teacher has now grown into a full-blown business (!!) that reaches millions of people with fun recipes each month.
+                                <br /><br />
+                                I live in Saint Paul, MN with my husband Bjork and our dog Sage. My favorite things in life are a big plate of pad Thai, sunny days, and going to the dog park.
+
                             </p>
-                            <p className="font-raleway leading-8">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing
-                                elit, sed do eiusmod tempor incididunt ut labore et
-                                dolore magna aliqua. Ut enim ad minim veniam, quis
-                                nostrud exercitation ullamco laboris nisi ut aliquip
-                                ex ea commodo consequat.Ut enim ad minim veniam, quis
-                                nostrud exercitation ullamco laboris nisi ut aliquip
-                                ex ea commodo consequat.
+                            <p className="text-xl m-6 font-raleway font-bold leading-8 text-center">
+                                My motto is: <br />
+                                If I wouldn’t eat it in real life, I won’t put in on the blog.
                             </p>
-                            <p className="text-xl m-6 font-raleway font-bold leading-8">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing
-                                elit.
-                            </p>
+
 
                         </div>
                         <div className="ml-12 mt-12">
