6b681a8b857ab558facfb9214c6230aeff59adbc
skarlov <56872071+skarlov@users.noreply.github.com>
2022-02-13 17:21:32 +0100

    Added components for store page

---
+++ components/add_to_cart_button.js
@@ -0,0 +1,20 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+import SpiderImg from '../assets/spider.png';
+
+const AddToCartButton = () => {
+    return (
+        <Link href="/">
+            <button className="w-auto py-3 rounded-lg bg-orange-400 hover:bg-orange-500 hover: shadow-md whitespace-nowrap text-xs text-black font-medium font-shadow border-0">
+
+                <div className="px-4 align-center flex flex-row justify-center">
+                    ADD TO CART
+
+                </div>
+            </button>
+        </Link>
+    );
+};
+
+export default AddToCartButton;
