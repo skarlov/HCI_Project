6012fcf0fac6755f2ae094c851031fcae9c3f864
skarlov <56872071+skarlov@users.noreply.github.com>
2022-02-13 20:02:35 +0100

    Added text to how_its_going_details.js

--- modules/how_its_going_details.js
+++ modules/how_its_going_details.js
@@ -29,53 +29,32 @@ const HowItsGoingDetails = () => {
 
                         </div>
                         <div className="mt-10 w-5/6 text-justify">
-                            <p className="font-raleway leading-8">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing
-                                elit, sed do eiusmod tempor incididunt ut labore et
-                                dolore magna aliqua. Ut enim ad minim veniam, quis
-                                nostrud exercitation ullamco laboris nisi ut aliquip
-                                ex ea commodo consequat.
+                            <p className="text-xl mb-4 font-raleway font-bold leading-8">
+                                10 things you may not know about me:
                             </p>
                             <p className="font-raleway leading-8">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing
-                                elit, sed do eiusmod tempor incididunt ut labore et
-                                dolore magna aliqua. Ut enim ad minim veniam, quis
-                                nostrud exercitation ullamco laboris nisi ut aliquip
-                                ex ea commodo consequat.Ut enim ad minim veniam, quis
-                                nostrud exercitation ullamco laboris nisi ut aliquip
-                                ex ea commodo consequat.
+                                1. I’m a Utah native who grew up with the mountains in my backyard but I always dreamed of living at the beach.
+                                <br />
+                                2. That’s why my husband and I moved to Venice, CA for 13 years. I created and designed magazines about natural health, bodybuilding, MMA fighters, and as one will do when one is in Hollywood, about celebrity stuff. It was like living on a permanent vacation. Then I got pregnant (surprise!) and had a baby. So we moved back to Utah and our families we love so much. And I couldn’t be happier to be home from vacation.
+                                <br />
+                                3. Our daughter’s nickname is Smudge. I started calling her that when she was in utero because she was just a little Smudge of love. And now at nearly 6’0″, she’s our biggest Smudge of love ever and our everything.
+                                <br />
+                                4. My husband is my secret weapon in the kitchen.
+                                <br />
+                                5. I seek out and explore grocery stores like other people visit art museums or fine boutiques.
+                                <br />
+                                6. I wish I was an amazing baker. I try and I try, and one day, I will be. My favorite sweets are German chocolate cake everything like this and this.
+                                <br />
+                                7. My last day on earth would include spending the entire day at the beach with my man, my Ali Smudge and a couple of dogs in tow. It would also have to include a cooler full of spicy tuna hand rolls and a couple of beers, while playing Smashball as the sun begins to set on my permanently smiling face.
+                                <br />
+                                8. I take a tub every, single, night. And my husband draws it for me. Spoiled I am, indeed.
+                                <br />
+                                9. I’m a to-do list and day planner/calendar junkie, and when I couldn’t find one that worked for me, I created my own.
+                                <br />
+                                10. My mom is my biggest cheerleader. And I love every one of her pom poms.
                             </p>
-                            <p className="text-xl m-6 font-raleway font-bold leading-8">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing
-                                elit.
-                            </p>
-                            <p className="font-raleway leading-8">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing
-                                elit, sed do eiusmod tempor incididunt ut labore et
-                                dolore magna aliqua. Ut enim ad minim veniam, quis
-                                nostrud exercitation ullamco laboris nisi ut aliquip
-                                ex ea commodo consequat.
-                            </p>
-                            <p className="font-raleway leading-8">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing
-                                elit, sed do eiusmod tempor incididunt ut labore et
-                                dolore magna aliqua. Ut enim ad minim veniam, quis
-                                nostrud exercitation ullamco laboris nisi ut aliquip
-                                ex ea commodo consequat.
-                            </p>
-                            <p className="font-raleway leading-8">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing
-                                elit, sed do eiusmod tempor incididunt ut labore et
-                                dolore magna aliqua. Ut enim ad minim veniam, quis
-                                nostrud exercitation ullamco laboris nisi ut aliquip
-                                ex ea commodo consequat. Ut enim ad minim veniam, quis
-                                nostrud exercitation ullamco laboris nisi ut aliquip
-                                ex ea commodo consequat.
-                            </p>
-                            <p className="text-xl m-6 font-raleway font-bold leading-8">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing
-                                elit.
-                            </p>
+
+
                         </div>
                     </div>
                 </div>
